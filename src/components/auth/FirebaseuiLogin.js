5985c9a04e7c66029fdd5eb4d30090c12044ef27
GreenSmile <greenvalera@gmail.com>
2021-04-13 17:55:42 +0300

    #Upgrade dependencies

--- src/components/auth/FirebaseuiLogin.js
+++ src/components/auth/FirebaseuiLogin.js
@@ -15,9 +15,7 @@ const Login = ({signInOptions, firebaseAuth}) => {
 
   return (
     <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth}/>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth}/>
     </div>
   );
 }
